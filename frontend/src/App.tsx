0e11163addbae391f170149734de9e372711437a
Sulochan Mahajan <sulochanmahajan@gmail.com>
2026-05-11 09:27:06 +0530

    setup : basic folder and dependencies setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='min-h-screen bg-black text-white'>
+      <h1 className='text-5xl'>Pulseboard</h1>
+    </div>
+  )
+}
+
+export default App
