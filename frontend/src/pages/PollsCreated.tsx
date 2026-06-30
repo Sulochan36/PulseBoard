a154a432fec6dae0c042fa92f51620caebf03a19
Sulochan Mahajan <sulochanmahajan@gmail.com>
2026-06-30 23:15:29 +0530

    fix: fix react import

--- frontend/src/pages/PollsCreated.tsx
+++ frontend/src/pages/PollsCreated.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import Container from '../components/layout/Container'
 import PollCard from '../components/PollCard'
 import { getMyPollsAPI } from '../services/poll.service';
